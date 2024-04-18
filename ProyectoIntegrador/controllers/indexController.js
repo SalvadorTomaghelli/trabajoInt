d41847736d8b9c100a247de49016f946da6b693c
SalvadorTomaghelli <stomaghelli@udesa.edu.ar>
2024-04-18 17:14:00 -0300

    ultimo cambio en el ruteo

--- ProyectoIntegrador/controllers/indexController.js
+++ ProyectoIntegrador/controllers/indexController.js
@@ -2,7 +2,9 @@ const indexController ={
     index: function(req, res, next) {
         res.render('index');
         },
-    
+    searchResults: function(req, res, next) {
+        res.render('search-results');
+        }
 }
 
 module.exports = indexController
--- ProyectoIntegrador/routes/index.js
+++ ProyectoIntegrador/routes/index.js
@@ -1,8 +1,7 @@
 var express = require('express');
 var router = express.Router();
 let indexController = require('../controllers/indexController')
-
 /* GET home page. */
 router.get('/', indexController.index);
-
+router.get('/searchResults', indexController.searchResults)
 module.exports = router;
