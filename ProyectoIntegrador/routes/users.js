8f9be0e2eccba3e3be94acfcad3097646bb59d42
Mateo Ruete <rmateo@udesa.edu.ar>
2024-04-19 10:12:10 -0300

    foto2

--- ProyectoIntegrador/routes/users.js
+++ ProyectoIntegrador/routes/users.js
@@ -2,6 +2,7 @@ var express = require('express');
 var router = express.Router();
 let usersController = require('../controllers/usersController')
 
+
 router.get('/', usersController.users);
 router.get('/login', usersController.login);
 router.get('/register', usersController.register);
