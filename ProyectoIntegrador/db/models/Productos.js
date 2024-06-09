3dc9cbadc83b30c830c6ab4fb43f400bc8b8f21e
mateoruete <mruete@udesa.edu.ar>
2024-06-09 19:53:07 -0300

    modelo product

---
+++ ProyectoIntegrador/db/models/Productos.js
@@ -0,0 +1,43 @@
+module.exports = function(sequelize,database){
+    let alias = 'Productos'
+    let cols = {
+        id:{
+            type: DataTypes.INTEGER,
+            primaryKey: true, 
+            autoIncrement: true
+        },
+        id_usuario:{
+            type: DataTypes.INTEGER
+        },
+        nombre_foto:{
+            type: dataTypes.STRING
+        },
+        nombre_producto:{
+            type: dataTypes.STRING
+        },
+        descripcion:{
+            type: dataTypes.STRING
+        },
+        createdAt:{
+            type: dataTypes.DATE,
+            allowNull:true
+        },
+        updatedAt:{
+            type: dataTypes.DATE,
+            allowNull: true
+        },
+        deletedAt:{
+            type: dataTypes.DATE,
+            allowNull: true
+        }
+
+    }
+    let config={
+        tablename: "Productos",
+        timestamps: false,
+        underscored: false
+    }
+    const Productos= sequelize.define(alias,cols,config);
+
+    return Productos
+}
