10704b78a20bc9b4cfda565b5804f6debf2a2fb0
SalvadorTomaghelli <stomaghelli@udesa.edu.ar>
2024-06-11 18:11:17 -0300

    register controller y routes incompleto

--- ProyectoIntegrador/routes/users.js
+++ ProyectoIntegrador/routes/users.js
@@ -6,8 +6,9 @@ const registerValidations=require('../middlewares/register-validator')
 
 router.get('/', usersController.users);
 router.get('/login', usersController.login);
-router.get('/register', usersController.register);
+// router.get('/register', usersController.register);
 router.get('/profile', usersController.profile);
 router.get('/profileEdit', usersController.profileEdit);
 //Rutas Post, Hay que icluir las validaciones entre el Path y el controller
+router.post('/register', registerValidations, usersController.register)
 module.exports = router;
